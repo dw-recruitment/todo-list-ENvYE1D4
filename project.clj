b8287849dc66c52772d5cc67c77605bf9f342079
Anonymous Candidate <anon@example.com>
2016-03-27 11:07:04 -0600

    Complete step 2
    - this needs tests and a headline

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [io.pedestal/pedestal.jetty "0.4.1"]
+                 [io.pedestal/pedestal.service "0.4.1"]
+                 [io.pedestal/pedestal.service-tools "0.4.1"]
+                 [ns-tracker "0.3.0"]]
+  :main ^{:skip-aot true} todo.core)
--- src/todo/core.clj
+++ src/todo/core.clj
@@ -1,6 +1,34 @@
-(ns todo.core)
+(ns todo.core
+  (:require [clojure.java.io :as io]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route :refer [router]]
+            [io.pedestal.http.route.definition :refer [defroutes]]
+            [ns-tracker.core :refer [ns-tracker]]
+            [ring.util.response :as ring-resp]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn home-page
+  ([req]
+   (let [gif (io/file (io/resource "public/under-construction.gif"))]
+     (assoc-in (ring-resp/response gif) [:headers "Content-Type"]
+               "image/gif"))))
+
+(defroutes routes
+  [[["/"
+     {:get home-page}]]])
+
+(def track-namespaces
+  (ns-tracker ["src"]))
+
+(def service {::http/interceptors [http/log-request
+                                   http/not-found
+                                   (router (fn []
+                                             (doseq [ns-sym (track-namespaces)]
+                                               (require ns-sym :reload))
+                                             routes))]
+              ::http/port 8080})
+
+(defn -main
+  [& args]
+  (-> service
+      http/create-server
+      http/start))
